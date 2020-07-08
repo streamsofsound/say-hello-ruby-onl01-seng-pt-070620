aaa1bcc142c7c108a44d4d7f683814769cb3b7dd
Rachel Bennett <rachellaurynbennett@gmail.com>
2020-07-08 01:40:09 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
-def greeting(name = "Ruby Programer")
-  puts "Hello #{name}!"
-end
-
 def say_hello
   puts "Hello Ruby Programer!"
+end
+
+def greeting(name = "Ruby Programer")
+  puts "Hello #{name}!"
 end
